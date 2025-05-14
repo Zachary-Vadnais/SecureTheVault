f9183a47c0c8c3d26b7bee3221b1740546e570a5
zachary vadnais <rsminigaming@gmail.com>
2025-05-14 01:30:18 -0400

    figured out what i have been doing wrong

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+let a = 7
+let B = 3
+let code1 = ('B'+'A')
+
+let code2 = ('x'/'y'-'z')
+let x = 2
+let y = 80 
+let z = 0
+
+
+let c = 2
+let D = 39
+let f = 34
+let code3 = ('C' % 'D'+ "f")
+
+const codeA = '${code1} ${code2} ${code3}'
+
+const codeB =('ab-bc-cd')
+let ab = 10
+let bc = 40
+let cd = 39
+
+console.log ('codeA')
+console.log('codeB')
